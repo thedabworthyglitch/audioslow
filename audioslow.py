09979fc002a26f5779144fe191b4cd5fbd071052
thedabworthyglitch <thedabworthyglitch@gmail.com>
2023-08-28 19:10:48 +0530

    sane sane sane

--- audioslow.py
+++ audioslow.py
@@ -20,33 +20,30 @@ def apply_reverb(audio, amount):
     return audio_with_reverb
 
 # Function to generate output filename based on manipulation
-def generate_output_filename(original_filename, speed_factor, reverb_strength):
+def generate_output_filename(original_filename, speed_factor):
     filename, extension = os.path.splitext(original_filename)
     speed_type = "slowed" if speed_factor < 1 else "sped_up"
-    reverb_str = f"_reverb_{int(reverb_strength * 100)}" if reverb_strength > 0 else ""
-    new_filename = f"{filename}_{speed_type}_{int(speed_factor * 100)}_percent{reverb_str}{extension}"
+    new_filename = f"{filename}_{speed_type}_{int(speed_factor * 100)}_percent{extension}"
     return new_filename
 
 # Function to manipulate audio
 def manipulate_audio():
     global original_audio, sample_rate
     speed_factor = speed_slider.get() / 100.0
-    reverb_strength = reverb_slider.get() / 100.0
+    reverb_amount = reverb_slider.get() / 100.0
 
     new_audio = change_speed(original_audio, sample_rate, speed_factor)
     
-    if reverb_strength > 0:
-        new_audio = apply_reverb(new_audio, reverb_strength)
+    if reverb_var.get():
+        new_audio = apply_reverb(new_audio, reverb_amount)
     
-    output_filename = generate_output_filename(original_filename, speed_factor, reverb_strength)
+    output_filename = generate_output_filename(original_filename, speed_factor)
     sf.write(output_filename, new_audio, sample_rate)
+    
+    # Update progress bar
+    progress_bar["value"] = 100  # Set to 100% when processing is complete
+    
     status_label.config(text="Audio manipulation completed and saved as: " + output_filename)
-    export_button.config(state=tk.NORMAL)  # Enable the export button
-
-# Function to update the progress bar
-def update_progress(value):
-    progress_bar["value"] = value
-    root.update_idletasks()
 
 # Function to select an audio file
 def select_audio_file():
@@ -55,8 +52,6 @@ def select_audio_file():
     original_audio, sample_rate = sf.read(file_path)
     original_filename = os.path.basename(file_path)
     status_label.config(text="Selected audio file: " + original_filename)
-    update_progress(0)  # Reset progress bar
-    export_button.config(state=tk.DISABLED)  # Disable the export button until manipulation is done
 
 # Create the main GUI window
 root = tk.Tk()
@@ -65,20 +60,23 @@ root.title("Audio Manipulation App")
 # Create UI components
 select_button = tk.Button(root, text="Select Audio File", command=select_audio_file)
 speed_slider = tk.Scale(root, from_=1, to=200, label="Speed %", orient="horizontal", length=300)
-reverb_slider = tk.Scale(root, from_=0, to=100, label="Reverb Strength %", orient="horizontal", length=300)
-manipulate_button = tk.Button(root, text="Process Audio", command=manipulate_audio)
-export_button = tk.Button(root, text="Export Audio", command=export_audio, state=tk.DISABLED)
-progress_bar = ttk.Progressbar(root, mode="determinate")
+reverb_var = tk.IntVar()
+reverb_checkbox = tk.Checkbutton(root, text="Enable Reverb", variable=reverb_var)
+reverb_slider = tk.Scale(root, from_=0, to=100, label="Reverb Amount %", orient="horizontal", length=300)
+manipulate_button = tk.Button(root, text="Manipulate Audio", command=manipulate_audio)
 status_label = tk.Label(root, text="", wraplength=400)
+
+# Create a progress bar widget
+progress_bar = ttk.Progressbar(root, orient="horizontal", length=300, mode="determinate")
 
 # Position UI components using grid layout
 select_button.grid(row=0, column=0, padx=10, pady=10)
 speed_slider.grid(row=1, column=0, padx=10, pady=10)
-reverb_slider.grid(row=2, column=0, padx=10, pady=10)
-manipulate_button.grid(row=3, column=0, padx=10, pady=10)
-export_button.grid(row=4, column=0, padx=10, pady=10)
-progress_bar.grid(row=5, column=0, padx=10, pady=10)
-status_label.grid(row=6, column=0, padx=10, pady=10)
+reverb_checkbox.grid(row=2, column=0, padx=10, pady=10)
+reverb_slider.grid(row=3, column=0, padx=10, pady=10)
+manipulate_button.grid(row=4, column=0, padx=10, pady=10)
+status_label.grid(row=5, column=0, padx=10, pady=10)
+progress_bar.grid(row=6, column=0, padx=10, pady=10)
 
 # Start the GUI event loop
 root.mainloop()
