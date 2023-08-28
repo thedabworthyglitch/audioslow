b31ee1f67332d8c431f16140c5565ddadba56c6d
thedabworthyglitch <thedabworthyglitch@gmail.com>
2023-08-28 19:12:19 +0530

    removing everything, going basic

--- audioslow.py
+++ audioslow.py
@@ -13,12 +13,6 @@ def change_speed(audio, sample_rate, speed_factor):
     new_audio = signal.resample(audio, new_num_samples)
     return new_audio
 
-# Function to apply reverb effect
-def apply_reverb(audio, amount):
-    reverb = np.random.normal(size=len(audio))
-    audio_with_reverb = audio + amount * reverb
-    return audio_with_reverb
-
 # Function to generate output filename based on manipulation
 def generate_output_filename(original_filename, speed_factor):
     filename, extension = os.path.splitext(original_filename)
@@ -30,12 +24,8 @@ def generate_output_filename(original_filename, speed_factor):
 def manipulate_audio():
     global original_audio, sample_rate
     speed_factor = speed_slider.get() / 100.0
-    reverb_amount = reverb_slider.get() / 100.0
 
     new_audio = change_speed(original_audio, sample_rate, speed_factor)
-    
-    if reverb_var.get():
-        new_audio = apply_reverb(new_audio, reverb_amount)
     
     output_filename = generate_output_filename(original_filename, speed_factor)
     sf.write(output_filename, new_audio, sample_rate)
@@ -60,9 +50,6 @@ root.title("Audio Manipulation App")
 # Create UI components
 select_button = tk.Button(root, text="Select Audio File", command=select_audio_file)
 speed_slider = tk.Scale(root, from_=1, to=200, label="Speed %", orient="horizontal", length=300)
-reverb_var = tk.IntVar()
-reverb_checkbox = tk.Checkbutton(root, text="Enable Reverb", variable=reverb_var)
-reverb_slider = tk.Scale(root, from_=0, to=100, label="Reverb Amount %", orient="horizontal", length=300)
 manipulate_button = tk.Button(root, text="Manipulate Audio", command=manipulate_audio)
 status_label = tk.Label(root, text="", wraplength=400)
 
@@ -72,11 +59,9 @@ progress_bar = ttk.Progressbar(root, orient="horizontal", length=300, mode="dete
 # Position UI components using grid layout
 select_button.grid(row=0, column=0, padx=10, pady=10)
 speed_slider.grid(row=1, column=0, padx=10, pady=10)
-reverb_checkbox.grid(row=2, column=0, padx=10, pady=10)
-reverb_slider.grid(row=3, column=0, padx=10, pady=10)
-manipulate_button.grid(row=4, column=0, padx=10, pady=10)
-status_label.grid(row=5, column=0, padx=10, pady=10)
-progress_bar.grid(row=6, column=0, padx=10, pady=10)
+manipulate_button.grid(row=2, column=0, padx=10, pady=10)
+status_label.grid(row=3, column=0, padx=10, pady=10)
+progress_bar.grid(row=4, column=0, padx=10, pady=10)
 
 # Start the GUI event loop
 root.mainloop()
